a82c5589e74b39ec4e59d2a60c136f3b20cd2007
gmc-code <gmccarthy@tpg.com.au>
2025-11-16 07:05:52 +1100

    lexer

--- docs/pseudocode_lexer.py
+++ docs/pseudocode_lexer.py
@@ -1,6 +1,5 @@
 from pygments.lexer import RegexLexer
-from pygments.token import Keyword, Name, Operator, String, Number, Text, Punctuation
-
+from pygments.token import Keyword, Name
 class PseudocodeLexer(RegexLexer):
     name = 'Pseudocode'
     aliases = ['pseudocode']
@@ -11,8 +10,8 @@ class PseudocodeLexer(RegexLexer):
             # Keywords
             (r'\b(MAIN PROGRAM|FUNCTION|ENDFUNCTION|END\s+FUNCTION|PROCEDURE|ENDPROCEDURE|END\s+PROCEDURE|CALL|RETURN|IF|THEN|ELSEIF|ELSE|ENDIF|CASE|OF|ENDCASE|FOR|TO|STEP|ENDFOR|WHILE|ENDWHILE|REPEAT|UNTIL|DO|INPUT|OUTPUT|PRINT|READ|WRITE|AND|OR|NOT|TRUE|FALSE|ARRAY|LIST|RECORD|DECLARE|SET|BEGIN|START|END|EXIT|BREAK|CONTINUE|IS|IS IN|)\b', Keyword),
 
-            # Identifiers
-            (r'[A-Z]*', Name),
+            # Any other ALL CAPS identifiers
+            (r'\b[A-Z][A-Z0-9_]*\b', Name),
 
         ],
     }
